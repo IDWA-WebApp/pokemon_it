2b7a9bf1e912c8525112814b62ea2eeccc477139
IDWA-WebApp <johny_dos@hotmail.com>
2022-03-25 00:58:13 +0200

    Update apoke.js

--- apoke.js
+++ apoke.js
@@ -80,8 +80,6 @@ function FavoriteCall(poke_id, irow, myhash) {
      ShowMessage('We are sorry! The action was not completed. Please try again. (Message: ' + xmlhttp.response + ')');
      return false;
     } else {
-     //document.getElementById('sLoading_' + i).style.visibility = 'hidden';
-     //document.getElementById('sLoading_' + i).style.display = 'none';
      var irow = xmlhttp.extraInfo
      var newtitlevalue = '';
      var favbutton = document.getElementById('fav-pokemon-' + irow);
